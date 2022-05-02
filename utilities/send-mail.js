206ef757e833f519a338c3c89ac2acc8ee539840
wanghn <1026932324@qq.com>
2022-05-03 00:31:53 +0800

    add email notice switch

--- utilities/send-mail.js
+++ utilities/send-mail.js
@@ -35,26 +35,28 @@ exports.notice = (comment) => {
   let POST_URL_QMSG = comment.get("url") + "#" + comment.get("objectId");
   let SITE_URL = process.env.SITE_URL;
 
-  let _template =
-    process.env.MAIL_TEMPLATE_ADMIN ||
-    '<div style="border-top:2px solid #12ADDB;box-shadow:0 1px 3px #AAAAAA;line-height:180%;padding:0 15px 12px;margin:50px auto;font-size:12px;"><h2 style="border-bottom:1px solid #DDD;font-size:14px;font-weight:normal;padding:13px 0 10px 8px;">        您在<a style="text-decoration:none;color: #12ADDB;" href="${SITE_URL}" target="_blank">${SITE_NAME}</a>上的文章有了新的评论</h2><p><strong>${NICK}</strong>回复说：</p><div style="background-color: #f5f5f5;padding: 10px 15px;margin:18px 0;word-wrap:break-word;">            ${COMMENT}</div><p>您可以点击<a style="text-decoration:none; color:#12addb" href="${POST_URL}" target="_blank">查看回复的完整內容</a><br></p></div></div>';
-  let _subject = process.env.MAIL_SUBJECT_ADMIN || "${SITE_NAME}上有新评论了";
-  let emailSubject = eval("`" + _subject + "`");
-  let emailContent = eval("`" + _template + "`");
+  if (process.env.EMAIL_NOTICE != null) {
+    let _template =
+      process.env.MAIL_TEMPLATE_ADMIN ||
+      '<div style="border-top:2px solid #12ADDB;box-shadow:0 1px 3px #AAAAAA;line-height:180%;padding:0 15px 12px;margin:50px auto;font-size:12px;"><h2 style="border-bottom:1px solid #DDD;font-size:14px;font-weight:normal;padding:13px 0 10px 8px;">        您在<a style="text-decoration:none;color: #12ADDB;" href="${SITE_URL}" target="_blank">${SITE_NAME}</a>上的文章有了新的评论</h2><p><strong>${NICK}</strong>回复说：</p><div style="background-color: #f5f5f5;padding: 10px 15px;margin:18px 0;word-wrap:break-word;">            ${COMMENT}</div><p>您可以点击<a style="text-decoration:none; color:#12addb" href="${POST_URL}" target="_blank">查看回复的完整內容</a><br></p></div></div>';
+    let _subject = process.env.MAIL_SUBJECT_ADMIN || "${SITE_NAME}上有新评论了";
+    let emailSubject = eval("`" + _subject + "`");
+    let emailContent = eval("`" + _template + "`");
 
-  let mailOptions = {
-    from: '"' + process.env.SENDER_NAME + '" <' + process.env.SENDER_EMAIL + ">",
-    to: process.env.BLOGGER_EMAIL || process.env.SENDER_EMAIL,
-    subject: emailSubject,
-    html: emailContent,
-  };
+    let mailOptions = {
+      from: '"' + process.env.SENDER_NAME + '" <' + process.env.SENDER_EMAIL + ">",
+      to: process.env.BLOGGER_EMAIL || process.env.SENDER_EMAIL,
+      subject: emailSubject,
+      html: emailContent,
+    };
 
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) return console.log(error);
-    console.log("博主通知邮件成功发送: %s", info.response);
-    comment.set("isNotified", true);
-    comment.save();
-  });
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) return console.log(error);
+      console.log("博主通知邮件成功发送: %s", info.response);
+      comment.set("isNotified", true);
+      comment.save();
+    });
+  }
 
   if (process.env.PUSHPLUS_TOKEN != null) {
     const   PushplusDespTemplate = `
@@ -93,7 +95,11 @@ exports.notice = (comment) => {
       function (error, response, body) {
         if (error) return console.log("发送微信提醒异常：", error);
         if (body) body = JSON.parse(body);
-        if (response.statusCode === 200 && body.code === 200) console.log("已发送微信提醒");
+        if (response.statusCode === 200 && body.code === 200) {
+          console.log("已发送微信提醒");
+          comment.set("isNotified", true);
+          comment.save();
+        }
         else console.warn("微信提醒失败:", body);
       }
     );
